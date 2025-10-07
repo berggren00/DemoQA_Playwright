d5d3d8bb104001787159e8a194f59749b162a2ba
Andreas Berggren <andreas.berggren@cobs.se>
2025-10-07 15:05:01 +0200

    added beforeEach + trying to figure out git workflows

--- tests/landing-page-smoke.spec.ts
+++ tests/landing-page-smoke.spec.ts
@@ -1,38 +1,36 @@
 import { test, expect } from "./fixtures/PageFixtures";
 
+test.beforeEach(async ({ landingPage }) => {
+  await landingPage.open()
+})
+
 
 test('open forms page', async ({ landingPage, page }) => {
-  await landingPage.open();
   await landingPage.goToSubPage("Forms");
   await expect(page).toHaveURL(/forms/);
 });
 
 test('open elements page', async ({ landingPage, page }) => {
-  await landingPage.open();
   await landingPage.goToSubPage("Elements");
   await expect(page).toHaveURL(/elements/);
 })
 
 test('open alerts page', async ({ landingPage, page }) => {
-  await landingPage.open();
   await landingPage.goToSubPage("Alerts, Frame & Windows");
   await expect(page).toHaveURL(/alertsWindow/);
 })
 
 test('open widgets page', async ({ landingPage, page }) => {
-  await landingPage.open();
   await landingPage.goToSubPage("Widgets");
   await expect(page).toHaveURL(/widgets/);
 })
 
 test('open interactions page', async ({ landingPage, page }) => {
-  await landingPage.open();
   await landingPage.goToSubPage("Interactions");
   await expect(page).toHaveURL(/interaction/);
 })
 
 test('open book store page', async ({ landingPage, page }) => {
-  await landingPage.open();
   await landingPage.goToSubPage("Book Store Application");
   await expect(page).toHaveURL(/books/);
 })
